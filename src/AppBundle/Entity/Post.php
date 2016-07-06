ff7762dbd771c5bbe726bc6079ece7893ffca5ad
Sébastien Kalinine <sebastien@diplomeo.fr>
2016-07-06 21:24:25 +0200

    Generate post and relations

---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Post
+ */
+class Post
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var bool
+     */
+    private $isMain;
+
+    /**
+     * @var bool
+     */
+    private $isEnd;
+
+    /**
+     * @var int
+     */
+    private $userId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set isMain
+     *
+     * @param boolean $isMain
+     *
+     * @return Post
+     */
+    public function setIsMain($isMain)
+    {
+        $this->isMain = $isMain;
+
+        return $this;
+    }
+
+    /**
+     * Get isMain
+     *
+     * @return bool
+     */
+    public function getIsMain()
+    {
+        return $this->isMain;
+    }
+
+    /**
+     * Set isEnd
+     *
+     * @param boolean $isEnd
+     *
+     * @return Post
+     */
+    public function setIsEnd($isEnd)
+    {
+        $this->isEnd = $isEnd;
+
+        return $this;
+    }
+
+    /**
+     * Get isEnd
+     *
+     * @return bool
+     */
+    public function getIsEnd()
+    {
+        return $this->isEnd;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return Post
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+}
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -173,4 +173,43 @@ class User extends BaseUser
     {
         return $this->lastName;
     }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $posts;
+
+
+    /**
+     * Add post
+     *
+     * @param \AppBundle\Entity\Post $post
+     *
+     * @return User
+     */
+    public function addPost(\AppBundle\Entity\Post $post)
+    {
+        $this->posts[] = $post;
+
+        return $this;
+    }
+
+    /**
+     * Remove post
+     *
+     * @param \AppBundle\Entity\Post $post
+     */
+    public function removePost(\AppBundle\Entity\Post $post)
+    {
+        $this->posts->removeElement($post);
+    }
+
+    /**
+     * Get posts
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
 }
