50fd8ca967bbc8e2c144ca12d87f8232bc92825f
Sébastien Kalinine <sebastien@diplomeo.fr>
2016-07-20 16:30:02 +0200

    Rename post controller

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class Post extends Controller
+class PostController extends Controller
 {
     /**
      * @Route("/contribution/{slug}", name="post_show")
