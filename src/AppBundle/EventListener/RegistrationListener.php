46b4055a67da8b8750736edd19dc729e0122a8d4
Sébastien Kalinine <sebastien@diplomeo.fr>
2016-09-20 00:01:19 +0200

    Improve register

---
+++ src/AppBundle/EventListener/RegistrationListener.php
@@ -0,0 +1,42 @@
+<?php
+namespace AppBundle\EventListener;
+
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class RegistrationListener implements EventSubscriberInterface
+{
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+                FOSUserEvents::REGISTRATION_CONFIRM     => 'onRegistrationConfirm',
+        );
+    }
+
+    public function onRegistrationConfirm(GetResponseUserEvent $event)
+    {
+        $url = $this->router->generate('fos_user_profile_show');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+
+    public function onRegistrationCompleted(GetResponseUserEvent $event)
+    {
+        $url = $this->router->generate('homepage');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
