98008f4091bbe2356c95c845867552017b1fa705
Sébastien KALININE <sebastien.kalinine@gmail.com>
2016-12-11 00:33:54 +0100

    Set min and max content size for posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 $( document ).ready(function() {
 
     tinymce.init({
-        // setup : function(ed){
-        //      ed.on('submit', function(e){
-        //         if (0 === ed.getContent().length)
-        //         {
-        //             console.log(this);
-        //             e.preventDefault();
-        //             alert('Le corps ne peut rester vide');
-
-        //             return false;
-        //         }
-        //      });
-        // },
-        selector: '.wysiwyg'
+        setup : function(ed){
+             ed.on('submit', function(e){
+                e.preventDefault();
+                if (460 > ed.plugins.wordcount.getCount())
+                {
+                    alert('Nombre de mots insuffisant');
+                }
+                if (1130 < ed.plugins.wordcount.getCount())
+                {
+                    alert('Nombre de mots trop grand');
+                }
+             });
+        },
+        selector: '.wysiwyg',
+        plugins: "wordcount"
     });
 });
