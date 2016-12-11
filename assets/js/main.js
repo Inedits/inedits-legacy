46827e087a306685ab367ab05a241a7b2b8b1f4e
Sébastien KALININE <sebastien.kalinine@gmail.com>
2016-12-11 03:18:46 +0100

    Fix min max post content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,20 @@
-$( document ).ready(function() {
+$( document ).ready(function($) {
 
     tinymce.init({
         setup : function(ed){
              ed.on('submit', function(e){
-                // e.preventDefault();
-                // if (460 > ed.plugins.wordcount.getCount())
-                // {
-                //     alert('Nombre de mots insuffisant');
-                // }
-                // if (1130 < ed.plugins.wordcount.getCount())
-                // {
-                //     alert('Nombre de mots trop grand');
-                // }
+                if ($('#app_post_new_file').prop('files').length === 0) {
+                    if (460 > ed.plugins.wordcount.getCount())
+                    {
+                        e.preventDefault();
+                        alert('Nombre de mots insuffisant');
+                    }
+                    if (1130 < ed.plugins.wordcount.getCount())
+                    {
+                        e.preventDefault();
+                        alert('Nombre de mots trop grand');
+                    }
+                }
              });
         },
         selector: '.wysiwyg',
