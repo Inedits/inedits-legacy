d80b6ca6c7ccfd759d1f2f072c235aa5ac8d5112
Sébastien KALININE <sebastien.kalinine@gmail.com>
2016-12-13 13:37:52 +0100

    Fix login check redirect route parameters

--- src/AppBundle/Security/LoginSuccessHandler.php
+++ src/AppBundle/Security/LoginSuccessHandler.php
@@ -22,11 +22,12 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface {
     public function onAuthenticationSuccess(Request $request, TokenInterface $token) {
 
         $response = null;
+        $user     = $token->getUser();
 
         if ($this->authorizationChecker->isGranted('ROLE_SUPER_ADMIN')) {
             $response = new RedirectResponse($this->router->generate('sonata_admin_redirect'));
         } else if ($this->authorizationChecker->isGranted('ROLE_USER')) {
-            $response = new RedirectResponse($this->router->generate('fos_user_profile_show'));
+            $response = new RedirectResponse($this->router->generate('fos_user_profile_show', [ 'slug' => $user->getSlug() ]));
         }
 
         return $response;
