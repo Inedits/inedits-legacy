a709f4fc3e652ab6ca1d073b8b86556bc3ae70ab
Sébastien KALININE <sebastien.kalinine@gmail.com>
2016-12-14 11:16:19 +0100

    Handle mailing alredy registered

--- src/AppBundle/Controller/MailingController.php
+++ src/AppBundle/Controller/MailingController.php
@@ -33,9 +33,16 @@ class MailingController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $mailing = $form->getData();
             $dupe    = $this->getDoctrine()->getRepository('AppBundle:Mailing')->findOneByMail($mailing->getMail());
+            $exist   = $this->getDoctrine()->getRepository('AppBundle:User')->findOneByMail($mailing->getMail());
 
             if ($dupe) {
                 $this->get('session')->getFlashBag()->add('danger', 'Cette adresse a déjà été sollicitées');
+                $referer = $request->headers->get('referer');
+
+                return $this->redirect($referer);
+            }
+            if ($exist) {
+                $this->get('session')->getFlashBag()->add('danger', 'Cette adresse est déjà inscrite sur notre site');
                 $referer = $request->headers->get('referer');
 
                 return $this->redirect($referer);
